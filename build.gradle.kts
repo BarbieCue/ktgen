6ab0be5c6799c9a9a046b49cbbfbc987efa9066d
René Knop <knopr@haufe.com>
2023-04-06 14:17:00 +0200

    add ability to build a fatjar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,21 @@
 plugins {
     kotlin("jvm") version "1.8.10"
     kotlin("plugin.serialization") version "1.8.10"
+    id("io.ktor.plugin") version "2.2.4"
 }
 
 group = "org.example"
 version = "0.0.1"
+
+application {
+    mainClass.set("org.example.ApplicationKt")
+}
+
+ktor {
+    fatJar {
+        archiveFileName.set("ktgen.jar")
+    }
+}
 
 repositories {
     mavenCentral()
