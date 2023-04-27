f0e8d8c4edcfd7d2518a5b7cbaec0898fce6a8cd
René Knop <knopr@haufe.com>
2023-04-27 09:17:39 +0200

    change dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ repositories {
     mavenCentral()
 }
 
-tasks.test {
+tasks.withType<Test>().configureEach {
     useJUnitPlatform()
 }
 
@@ -38,11 +38,10 @@ dependencies {
     implementation("io.github.pdvrieze.xmlutil:serialization-jvm:0.85.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.5.0")
 
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.8.10")
+    testImplementation("io.kotest:kotest-runner-junit5:5.5.5")
     testImplementation("io.kotest:kotest-assertions-json-jvm:5.5.5")
     testImplementation("io.kotest:kotest-assertions-core-jvm:5.5.5")
     testImplementation("io.kotest:kotest-property:5.5.5")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
     testImplementation("io.ktor:ktor-server-core-jvm:2.2.4")
     testImplementation("io.ktor:ktor-server-netty-jvm:2.2.4")
     testImplementation("ch.qos.logback:logback-classic:1.4.6")
