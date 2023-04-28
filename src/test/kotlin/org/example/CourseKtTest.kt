135de80efff47622a4e6deb236f43a0e2ddbb180
René Knop <knopr@haufe.com>
2023-04-28 14:06:52 +0200

    finish migration of CourseKtTest

--- src/test/kotlin/org/example/CourseKtTest.kt
+++ src/test/kotlin/org/example/CourseKtTest.kt
@@ -4,11 +4,11 @@ import io.kotest.assertions.throwables.shouldNotThrow
 import io.kotest.inspectors.forAll
 import io.kotest.matchers.collections.shouldHaveAtLeastSize
 import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.ints.shouldBeGreaterThanOrEqual
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldHaveLength
 import io.kotest.matchers.string.shouldHaveMaxLength
 import io.kotest.matchers.string.shouldHaveMinLength
-import org.junit.jupiter.api.Test
 import java.nio.file.attribute.PosixFilePermissions
 import java.util.*
 import kotlin.io.path.absolutePathString
@@ -37,21 +37,6 @@ class CourseKtTest : IOExpectSpec({
 
         expect("empty collection when path is empty") {
             readCourseSymbols("") shouldBe emptyList()
-        }
-
-        expect("") {
-        }
-
-        expect("") {
-        }
-
-        expect("") {
-        }
-
-        expect("") {
-        }
-
-        expect("") {
         }
     }
 
@@ -115,10 +100,16 @@ class CourseKtTest : IOExpectSpec({
             course.description shouldBe "Visit ktgen on Github (https://github.com/BarbieCue/ktgen)"
         }
 
-        expect("course consists of specified lessons") {
+        expect("course consists of lessons containing the specified symbols") {
             val courseSymbols = listOf("ab", "cd")
             val course = createCourse(courseSymbols, emptyList(), 20, 100)
             course.lessons.joinToString("") { it.newCharacters } shouldBe "abcd"
+        }
+
+        expect("course has no lessons when there are no specified symbols") {
+            val courseSymbols = emptyList<String>()
+            val course = createCourse(courseSymbols, emptyList(), 20, 100)
+            course.lessons shouldBe emptyList()
         }
 
         expect("each lesson text contains exactly the specified amount of non-whitespace symbols (symbols-per-lesson)") {
@@ -143,81 +134,49 @@ class CourseKtTest : IOExpectSpec({
                 }
             }
         }
+
+        expect("course contains word lessons when a non-empty dictionary is passed") {
+            val courseSymbols = listOf("ab", "cd", "ef", "gh")
+            val dictionary = listOf("gag")
+            val course = createCourse(courseSymbols, dictionary, 20, 100)
+            course.lessons shouldHaveAtLeastSize 1
+            course.lessons.count { it.text.contains("gag") } shouldBeGreaterThanOrEqual 1
+        }
+
+        expect("line-length range test") {
+            val courseSymbols = listOf("ab")
+            val symbolsPerLesson = 200
+            createCourse(courseSymbols, emptyList(), -100, symbolsPerLesson).lessons shouldHaveSize 0
+            createCourse(courseSymbols, emptyList(), -1, symbolsPerLesson).lessons shouldHaveSize 0
+            createCourse(courseSymbols, emptyList(), 0, symbolsPerLesson).lessons shouldHaveSize 0
+
+            createCourse(courseSymbols, emptyList(), 1, symbolsPerLesson).lessons.size shouldBeGreaterThanOrEqual 1
+            createCourse(courseSymbols, emptyList(), 1, symbolsPerLesson).lessons.forAll {
+                it.text.split('\n') shouldHaveSize 200
+                it.text.split('\n').forAll { line -> line shouldHaveLength 1 }
+            }
+
+            createCourse(courseSymbols, emptyList(), 100, symbolsPerLesson).lessons.size shouldBeGreaterThanOrEqual 1
+            createCourse(courseSymbols, emptyList(), 100, symbolsPerLesson).lessons.forAll {
+                it.text.split('\n') shouldHaveAtLeastSize 1
+                it.text.split('\n').dropLast(1) // last line can be shorter
+                    .forAll { line ->
+                        line shouldHaveMinLength 99
+                        line shouldHaveMaxLength 100 } }
+        }
+
+        expect("symbols-per-lesson range test") {
+            val courseSymbols = listOf("ab")
+            val lineLength = 100
+            createCourse(courseSymbols, emptyList(), lineLength, -100).lessons shouldHaveSize 0
+            createCourse(courseSymbols, emptyList(), lineLength, -1).lessons shouldHaveSize 0
+            createCourse(courseSymbols, emptyList(), lineLength, 0).lessons shouldHaveSize 0
+
+            createCourse(courseSymbols, emptyList(), lineLength, 1).lessons.size shouldBeGreaterThanOrEqual 1
+            createCourse(courseSymbols, emptyList(), lineLength, 1).lessons.forAll { it.text.count { char -> !char.isWhitespace() } shouldBe  1 }
+
+            createCourse(courseSymbols, emptyList(), lineLength, 100).lessons.size shouldBeGreaterThanOrEqual 1
+            createCourse(courseSymbols, emptyList(), lineLength, 100).lessons.forAll { it.text.count { char -> !char.isWhitespace() } shouldBe  100 }
+        }
     }
 })
-
-class CourseKtTestctiectiectie : FileTest() {
-
-    @Test
-    fun `createCourse happy`() {
-        val courseSymbols = listOf("ab", "cd")
-        val dictionary = listOf("apple", "pear", "grape", "cd", "in", "nice", "ai", "ab")
-        val lineLength = 20
-        val symbolsPerLesson = 100
-
-        val course = createCourse(courseSymbols, dictionary, lineLength, symbolsPerLesson)
-
-        course.lessons shouldHaveAtLeastSize 2
-
-        course.lessons[0].text.split('\n').forAll { it shouldHaveMaxLength 20 }
-        course.lessons[0].text.count { !it.isWhitespace() } shouldBe 100
-        course.lessons[1].text.split('\n').forAll { it shouldHaveMaxLength 20 }
-        course.lessons[1].text.count { !it.isWhitespace() } shouldBe 100
-    }
-
-    @Test
-    fun `createCourse empty course symbols`() {
-        val courseSymbols = emptyList<String>()
-        val dictionary = listOf("apple", "pear", "grape", "cd", "in", "nice", "ai", "ab")
-        val lineLength = 20
-        val wordsPerLesson = 10
-
-        val course = createCourse(courseSymbols, dictionary, lineLength, wordsPerLesson)
-        course.lessons shouldBe emptyList()
-    }
-
-    @Test
-    fun `createCourse empty dictionary`() {
-        val courseSymbols = listOf("ab", "cd")
-        val dictionary = emptyList<String>()
-        val lineLength = 20
-        val symbolsPerLesson = 40
-
-        val course = createCourse(courseSymbols, dictionary, lineLength, symbolsPerLesson)
-
-        // some lessons with random symbol permutations should be created
-        course.lessons shouldHaveAtLeastSize 2
-    }
-
-    @Test
-    fun `createCourse line length range test`() {
-        val courseSymbols = listOf("ab", "cd")
-        val dictionary = listOf("apple", "pear", "grape", "cd", "in", "nice", "ai", "ab")
-        val symbolsPerLesson = 200
-
-        createCourse(courseSymbols, dictionary, -100, symbolsPerLesson).lessons.forAll { it.text.split('\n').forAll { line -> line shouldHaveLength 0 } }
-        createCourse(courseSymbols, dictionary, -1, symbolsPerLesson).lessons.forAll { it.text.split('\n').forAll { line -> line shouldHaveLength 0 } }
-        createCourse(courseSymbols, dictionary, 0, symbolsPerLesson).lessons.forAll { it.text.split('\n').forAll { line -> line shouldHaveLength 0 } }
-        createCourse(courseSymbols, dictionary, 1, symbolsPerLesson).lessons.forAll { it.text.split('\n').forAll { line ->
-            line shouldHaveMinLength 0
-            line shouldHaveMaxLength 1 } }
-        createCourse(courseSymbols, dictionary, 100, symbolsPerLesson).lessons.forAll { it.text.split('\n')
-            .dropLast(1) // last line can be shorter
-            .forAll { line ->
-                line shouldHaveMinLength 99
-                line shouldHaveMaxLength 100 } }
-    }
-
-    @Test
-    fun `createCourse symbols per lesson range test`() {
-        val courseSymbols = listOf("ab")
-        val dictionary = emptyList<String>()
-        val lineLength = 100
-
-        createCourse(courseSymbols, dictionary, lineLength, -100).lessons shouldHaveSize 0
-        createCourse(courseSymbols, dictionary, lineLength, -1).lessons shouldHaveSize 0
-        createCourse(courseSymbols, dictionary, lineLength, 0).lessons shouldHaveSize 0
-        createCourse(courseSymbols, dictionary, lineLength, 1).lessons.forAll { it.text.count { char -> !char.isWhitespace() } shouldBe  1 }
-        createCourse(courseSymbols, dictionary, lineLength, 100).lessons.forAll { it.text.count { char -> !char.isWhitespace() } shouldBe  100 }
-    }
-}
