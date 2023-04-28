a62d6a809557da23a5569c8df46b78941496f027
René Knop <knopr@haufe.com>
2023-04-28 10:25:13 +0200

    add server handling to spec

--- src/test/kotlin/org/example/TestUtils.kt
+++ src/test/kotlin/org/example/TestUtils.kt
@@ -1,6 +1,9 @@
 package org.example
 
 import io.kotest.core.spec.style.ExpectSpec
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.TestInstance
 import java.nio.file.Path
@@ -33,6 +36,7 @@ abstract class TempFileExpectSpec(body: TempFileExpectSpec.() -> Unit = {}) : Ex
     init {
         body()
         afterEach { files.forEach { it.deleteIfExists() } }
+        afterEach { server.forEach { it.stop() } }
     }
 
     private val files = mutableListOf<Path>()
@@ -41,5 +45,13 @@ abstract class TempFileExpectSpec(body: TempFileExpectSpec.() -> Unit = {}) : Ex
         val file = kotlin.io.path.createTempFile(prefix = name, attributes = attributes)
         files.add(file)
         return file
+    }
+
+    private val server = mutableListOf<ApplicationEngine>()
+
+    fun startLocalhostWebServer(port: Int, module: Application.() -> Unit) {
+        val s = embeddedServer(Netty, port, host = "0.0.0.0", module = module)
+        s.start(false)
+        server.add(s)
     }
 }
