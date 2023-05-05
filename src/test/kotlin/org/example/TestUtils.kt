048691406236c2263c4a5a74d31f6791831aeeb5
René Knop <knopr@haufe.com>
2023-05-05 09:25:41 +0200

    remove obsolete code

--- src/test/kotlin/org/example/TestUtils.kt
+++ src/test/kotlin/org/example/TestUtils.kt
@@ -4,33 +4,9 @@ import io.kotest.core.spec.style.ExpectSpec
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import org.junit.jupiter.api.AfterAll
-import org.junit.jupiter.api.TestInstance
 import java.nio.file.Path
 import java.nio.file.attribute.FileAttribute
 import kotlin.io.path.deleteIfExists
-
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-abstract class FileTest {
-
-    init {
-        TODO("delete me!")
-    }
-
-    private val files = mutableListOf<Path>()
-
-    internal fun tmpFile(name: String, vararg attributes: FileAttribute<*>): Path {
-        val file = kotlin.io.path.createTempFile(prefix = name, attributes = attributes)
-        files.add(file)
-        return file
-    }
-
-    @AfterAll
-    fun deleteFiles() {
-        files.forEach { it.deleteIfExists() }
-    }
-}
-
 
 abstract class IOExpectSpec(body: IOExpectSpec.() -> Unit = {}) : ExpectSpec() {
     init {
